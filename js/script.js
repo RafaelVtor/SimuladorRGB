378aa246205ad88e9e7f111edb7844bd5ed3c0a8
Rafael Alves <rafaelvitor_as@hotmail.com>
2020-07-20 22:08:08 -0300

    Refatoração do código.

--- js/script.js
+++ js/script.js
@@ -6,28 +6,28 @@ function start() {
     var rangerGreen = document.querySelector('#green')
     var rangerBlue = document.querySelector('#blue')
 
-    rangerRed.focus()
+    var inputRed = document.querySelector('#redText')
+    var inputGreen = document.querySelector('#greenText')
+    var inputBlue = document.querySelector('#blueText')
 
-    rangerRed.addEventListener('input', eventChangeRanger)
-    rangerGreen.addEventListener('input', eventChangeRanger)
-    rangerBlue.addEventListener('input', eventChangeRanger)
+    rangerRed.addEventListener('input', eventChangeColor)
+    rangerGreen.addEventListener('input', eventChangeColor)
+    rangerBlue.addEventListener('input', eventChangeColor)
 
-    function eventChangeRanger() {
-
-        var inputRed = document.querySelector('#redText')
-        var inputGreen = document.querySelector('#greenText')
-        var inputBlue = document.querySelector('#blueText')
+    function eventChangeColor() {
 
         inputRed.value = rangerRed.value
         inputGreen.value = rangerGreen.value
         inputBlue.value = rangerBlue.value
 
-        eventChangeColor(rangerRed.value, rangerGreen.value, rangerBlue.value)
+        changeRanger()
+
     }
-    function eventChangeColor(red, green, blue) {
+
+    function changeRanger() {
 
         var color = document.querySelector('.color')
-        color.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
+        color.style.backgroundColor = `rgb(${rangerRed.value}, ${rangerGreen.value}, ${rangerBlue.value})`
 
     }
 
